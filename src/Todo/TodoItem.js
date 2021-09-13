6d1116a61a070a35262ea5904e9a90e4fa6981ce
JusticeUA <vasya.ganec@gmail.com>
2021-09-13 20:19:31 +0300

    first

---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+
+ function TodoItem({todo, index}) {
+return (
+    <li><strong>{index + 1} </strong>{todo.title}</li>
+)
+}
+
+
+
+export default TodoItem
